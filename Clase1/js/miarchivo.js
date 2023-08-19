3dd79e09c8842a4c8760c9a5bb1ff89c7bfec86b
Tammy Pardo <ta.pardotroncoso@gmail.com>
2023-08-19 17:57:06 -0400

    PreEntrega2TammyPardo

---
+++ Clase1/js/miarchivo.js
@@ -0,0 +1,67 @@
+//comentario de linea
+/*
+comentario
+de
+bloque
+*/
+
+//forma recomendada para declarar variables
+//let edad = 15;
+//let nombre = "andres";
+
+//forma no recomndadada
+//var numero = 4;
+
+
+//constantes
+//const NUMERO = 5;
+
+//edad = 20;
+
+//let numeroUno = 3;
+//let numeroDos = 5;
+ 
+//let resultadoSuma = numeroUno + numeroDos;
+//console.log("Resultado Suma", resultadoSuma);
+
+
+// Pedir número mediante prompt y si es mayor a 1000 mostrar un alert
+// var numero = prompt("Ingrese un número:");
+// if (numero > 1000) {
+//     alert("El número ingresado es mayor a 1000");
+// }
+
+// Pedir un texto mediante prompt, y si es igual a "Hola" mostrar un alerta por consola
+// var texto = prompt("Ingrese un texto:");
+// if (texto === "Hola") {
+//     console.log("El texto ingresado es igual a 'Hola'");
+// }
+
+// Pedir un número por prompt y evaluar si está entre 10 y 50. En caso positivo mostrar un alert
+// var numero2 = prompt("Ingrese otro número:");
+// if (numero2 >= 10 && numero2 <= 50) {
+//     alert("El número ingresado está entre 10 y 50");
+// }
+
+
+
+// let num = parseInt(prompt("Ingrese un número"));
+// for(let i = 1; i <= 10; i++){
+//   console.log(num + i);
+// }
+
+
+
+
+// let texto = "";
+// while(texto !== "ESC"){
+//   texto = prompt("Ingrese un texto");
+//   console.log(texto + " valor agregado");
+// }
+
+
+let texto = "";
+while(texto !== "ESC"){
+  texto = prompt("Ingrese un texto");
+  console.log(texto + " valor agregado");
+}
